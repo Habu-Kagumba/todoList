c6b0e2f6cdf61dd1464f57b047db7ff7dfb9fa3a
Herbert Kagumba <habukagumba@gmail.com>
2016-08-07 18:10:46 +0300

    feat(setup): Setup initial todolist application
    [#127806653]

---
+++ config/application.rb
@@ -0,0 +1,10 @@
+require "rapid_runty"
+
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require "todos_controller"
+
+module TodoList
+  class Application < RapidRunty::Application
+  end
+end
